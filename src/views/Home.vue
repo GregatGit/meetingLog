3379f8788ca2c31dcd44b27223d38d2cba19d3bc
gregatgit <gregdd@outlook.com>
2019-12-19 10:08:41 +1000

    use an icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container text-center">
+  <div class="container text-center">
     <div class="row justify-content-center">
       <div class="col-10 col-md-10 col-lg-8 col-xl-7">
         <h4 class="display-4 text-primary mt-3 mb-2">Meeting Log</h4>
@@ -8,23 +8,17 @@
           picks random users to award giveaways. It's a good example of a
           Single Page Application which includes connection to a database and
           routing. It's a practical way to learn
-          <a href="https://vuejs.org/">Vue.js</a>
+          <a
+            href="https://vuejs.org/"
+          >Vue.js</a>
           with
           <a href="https://firebase.google.com">Firebase</a>.
+          <font-awesome-icon icon="star"></font-awesome-icon>
         </p>
 
-        <router-link
-          class="btn btn-outline-primary mr-2"
-          to="/register"
-        >Register</router-link>
-        <router-link
-          class="btn btn-outline-primary mr-2"
-          to="/login"
-        >Log In</router-link>
-        <router-link
-          class="btn btn-primary"
-          to="/meetings"
-        >Meetings</router-link>
+        <router-link class="btn btn-outline-primary mr-2" to="/register">Register</router-link>
+        <router-link class="btn btn-outline-primary mr-2" to="/login">Log In</router-link>
+        <router-link class="btn btn-primary" to="/meetings">Meetings</router-link>
       </div>
     </div>
   </div>
@@ -32,10 +26,12 @@
 
 <script>
 // @ is an alias to /src
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 export default {
   name: 'home',
-  // components: {
-  //   HelloWorld,
-  // },
+  components: {
+    FontAwesomeIcon,
+  },
 }
 </script>
