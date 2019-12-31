69883409761487319492cc533912d065b3063217
gregatgit <gregdd@outlook.com>
2019-12-31 10:55:25 +1000

    tidy up

--- src/views/CheckIn.vue
+++ src/views/CheckIn.vue
@@ -7,13 +7,25 @@
             <div class="card-body">
               <h3 class="font-weight-light mb-3">Check in</h3>
               <section class="form-group">
-                <div class="col-12 alert alert-danger px-3">error</div>
+                <div class="col-12 alert alert-danger px-3" v-if="error">{{ error }}</div>
                 <label class="form-control-label sr-only" for="displayName">Name</label>
-                <input required class="form-control" type="text" placeholder="Name" v-model="displayName" />
+                <input
+                  required
+                  class="form-control"
+                  type="text"
+                  placeholder="Name"
+                  v-model="displayName"
+                />
               </section>
               <section class="form-group">
                 <label class="form-control-label sr-only" for="Email">Email</label>
-                <input required class="form-control" type="email" placeholder="Email" v-model="eMail" />
+                <input
+                  required
+                  class="form-control"
+                  type="email"
+                  placeholder="Email"
+                  v-model="eMail"
+                />
               </section>
               <div class="form-group text-right mb-0">
                 <button class="btn btn-primary" type="submit">Check in</button>
@@ -27,15 +39,22 @@
 </template>
 <script>
 export default {
+  name: 'checkIn',
   data: function() {
-    return { displayName: '', eMail: null }
+    return { displayName: null, eMail: null }
   },
   methods: {
     handleCheckIn: function() {
-      console.log('params', this.$route.params.meetingID)
+      this.$emit('checkIn', {
+        userID: this.$route.params.userID,
+        meetingID: this.$route.params.meetingID,
+        displayName: this.displayName,
+        eMail: this.eMail,
+      })
       this.displayName = null
       this.eMail = null
     },
   },
+  props: ['error'],
 }
 </script>
