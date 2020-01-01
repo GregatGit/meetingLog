328642b518aeedc5923a2dfd09b62f97f0c9b572
gregatgit <gregdd@outlook.com>
2020-01-01 16:27:07 +1000

    add meeting buttons and functionality

--- src/views/Attendees.vue
+++ src/views/Attendees.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="container mt-4">
     <div class="row justify-content-center">
-      <div class="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1" v-for="item in attendees" :key="item.invalid-feedback">
+      <div 
+        class="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1" v-for="item in attendees" 
+        :key="item.invalid-feedback"
+      >
         <div class="card">
           <div class="card-body px-3 py-2 d-flex align-items-center justify-content-center">
+            <div class="btn-group pr-2" v-if="user !== null && user.uid == userID">
+              <button 
+                class="btn btn-sm" 
+                :class="[
+                  item.star ? 'text-danger' : '', 'btn-outline-secondary'
+                ]"
+                title="Give user a star"
+                @click="toggleStar(item.id)"
+              >
+                <font-awesome-icon icon="star"></font-awesome-icon>
+              </button>
+              <a 
+                class="btn btn-sm btn-outline-secondary" title="Send user an email"
+                :href="'mailto:' + item.eMail"
+              >
+                <font-awesome-icon icon="envelope"></font-awesome-icon>
+              </a>
+              <button 
+                class="btn btn-sm btn-outline-secondary" title="delete attendee"
+                @click="deleteAttendee(item.id)"
+              >
+                <font-awesome-icon icon="trash"></font-awesome-icon>
+              </button>
+            </div>
             <div>{{item.displayName}}</div>
           </div>
         </div>
@@ -13,7 +40,9 @@
 </template>
 
 <script>
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import db from '../db'
+
 export default {
   name: 'Attendees',
   data: function() {
@@ -23,6 +52,47 @@ export default {
       meetingID: this.$route.params.meetingID,
     }
   },
+  components: {
+    FontAwesomeIcon
+  },
+  methods: {
+    deleteAttendee: function(attendeeID){
+      if (this.user && this.user.uid == this.userID){
+        db.collection('users')
+          .doc(this.user.uid)
+          .collection('meetings')
+          .doc(this.meetingID)
+          .collection('attendees')
+          .doc(attendeeID)
+          .delete()
+      }
+    },
+    toggleStar: function(attendeeID){
+      if (this.user && this.user.uid == this.userID){
+        const ref = db
+          .collection('users')
+          .doc(this.user.uid)
+          .collection('meetings')
+          .doc(this.meetingID)
+          .collection('attendees')
+          .doc(attendeeID)
+        
+        ref.get().then(doc => {
+          const star = doc.data().star
+          if (star) {
+            ref.update({
+              star: !star
+            })
+          } else {
+            ref.update({
+              star: true
+            })
+          }
+        })
+      }
+    }
+  },
+  props: ['user'],
   mounted() {
     db.collection('users')
       .doc(this.userID)
@@ -36,6 +106,7 @@ export default {
             id: doc.id,
             eMail: doc.data().eMail,
             displayName: doc.data().displayName,
+            star: doc.data().star
           })
         })
         this.attendees = snapData
